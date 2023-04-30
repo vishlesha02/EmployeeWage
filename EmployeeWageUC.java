6304bff9a7e34887b4fdfdbf00043ddd59c329b1
vishlesha02 <vishleshamortale18@gmail.com>
2023-04-30 22:02:19 +0530

    first commit

---
+++ EmployeeWageUC.java
@@ -0,0 +1,19 @@
+
+package Day7;
+
+public class EmployeeWageUC {
+    public static void main(String[] args) {
+
+
+        int IS_FULL_TIME = 1;
+        double empcheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empcheck == IS_FULL_TIME) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
+
+
+    }
+}
