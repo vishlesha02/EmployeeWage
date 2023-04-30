f518ed2db82177e09d5a89bc9e4aa3bf4117fcf9
vishlesha02 <vishleshamortale18@gmail.com>
2023-05-01 00:00:22 +0530

    EmployeeWageUC

--- EmployeeWageUC.java
+++ EmployeeWageUC.java
@@ -4,11 +4,9 @@ package Day7;
 public class EmployeeWageUC {
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
-    public static final int Emp_RATE_PER_HR = 20;
-    public static final int NUM_OF_WORKING_DAYS=20;
-    public static final int MAX_HR_IN_MONTH=100;
 
-    public  void computeWage() {
+    public static void computeWage(String company, int Emp_RATE_PER_HR, int NUM_OF_WORKING_DAYS, int MAX_HR_IN_MONTH ) {
+
         int EmpHr = 0;
         int day=1;
         int Total_Emp_Hr=0;
@@ -31,15 +29,13 @@ public class EmployeeWageUC {
 
                 default:
                     EmpHr = 0;
-
-
             }
+
             EmpWage = EmpHr * Emp_RATE_PER_HR;
             totalEmpWage += EmpWage;
             Total_Emp_Hr += EmpHr;
 
             System.out.println("Day " + day + " Employee Wage : " + EmpWage + "    Total Working Hr : " + Total_Emp_Hr);
-
             day++;
         }
         System.out.println("Toatal Emp Wage : " + totalEmpWage);
@@ -47,6 +43,7 @@ public class EmployeeWageUC {
 
     public static void main(String[] args) {
         EmployeeWageUC e=new EmployeeWageUC();
-        e.computeWage();
+        computeWage("Jio",10,15,15);
+        computeWage("Reliance", 20 , 2 , 10);
     }
 }
